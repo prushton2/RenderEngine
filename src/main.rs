682e6e1e446a8a6ac413ef280b5fa652218da7ad
prushton2 <peterrushton418@gmail.com>
2021-10-28 11:48:45 -0400

    implementing position modules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+mod position;
 
+fn main() {
+    let pos1 = position::Vector3::new(1.0, 2.0, 3.0);
+    let pos2 = position::Vector3::new(1.0, 2.0, 3.0);
+
+    let angle = position::Angle::new(10.0);
+
+    let sum = pos1.add(pos2);
+    dbg!(&sum.x);
+    dbg!(&sum.y);
+    dbg!(&sum.z);
+
+    dbg!(&angle.angle);
 }
