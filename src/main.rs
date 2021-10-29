3f97b048fade583abe491b00c555adb0a204b784
prushton2 <peterrushton418@gmail.com>
2021-10-29 09:39:55 -0400

    Updates main

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ mod object;
 
 fn main() {
 
-    let square = object::Square::new(
+    let cube = object::Cube::new(
         position::Vector3::new(0.0, 0.0, 0.0),
-        position::Vector3::new(0.0, 1.0, 0.0),
-        position::Vector3::new(1.0, 0.0, 0.0),
-        position::Vector3::new(1.0, 1.0, 0.0),
+        position::Vector3::new(1.0, 1.0, 1.0),
     );
 }
