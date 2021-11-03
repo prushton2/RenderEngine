c149af5356b8e680cb90cd0086e8407c95a739d9
prushton2 <peterrushton52@gmail.com>
2021-11-03 09:45:34 -0400

    added greater and less than; fixed normal math bugs

--- src/position/Angle.rs
+++ src/position/Angle.rs
@@ -11,23 +11,63 @@ impl angle {
         angle{angle:self.angle}
     }
     pub fn add(&self, other: &angle) -> angle {
+        let mut result = self.angle + other.angle;
+        while(result < 0.0) {
+            result += 360.0
+        }
         angle{
-            angle: (self.angle + other.angle) % 360.0
+            angle: result % 360.0
         }
     }
     pub fn sub(&self, other: &angle) -> angle {
+        let mut result = self.angle - other.angle;
+        while(result < 0.0) {
+            result += 360.0
+        }
         angle{
-            angle: (self.angle - other.angle) % 360.0
+            angle: result % 360.0
         }
     }
     pub fn mult(&self, other: &angle) -> angle {
+        let mut result = self.angle * other.angle;
+        while(result < 0.0) {
+            result += 360.0
+        }
         angle{
-            angle: (self.angle * other.angle) % 360.0
+            angle: result % 360.0
         }
     }
     pub fn div(&self, other: &angle) -> angle {
+        let mut result = self.angle / other.angle;
+        while(result < 0.0) {
+            result += 360.0
+        }
         angle{
-            angle: (self.angle / other.angle) % 360.0
+            angle: result % 360.0
+        }
+    }
+    pub fn gt(&self, other: &angle) -> bool {
+        let angle = self.angle;
+        let opposite = self.sub(&new(180.0));
+        let opposite = opposite.angle;
+        
+        if angle - opposite <= 0.0 { 
+            other.angle > opposite || other.angle < angle
+        }
+        else {
+            other.angle > opposite && other.angle < angle
+        }
+    }
+    pub fn lt(&self, other: &angle) -> bool {
+        let angle = self.angle;
+        let opposite = self.sub(&new(180.0));
+        let opposite = opposite.angle;
+        
+        if opposite - angle <= 0.0 { 
+            other.angle < opposite || other.angle > angle
+        }
+        else {
+            other.angle < opposite && other.angle > angle
         }
     }
 }
