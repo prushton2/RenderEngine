72daf49f3804d0ccaa4450996b8b4644ad62439e
prushton2 <peterrushton418@gmail.com>
2026-03-13 16:47:59 -0400

    Accidentally increased fps by read locking player less times per frame

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::sync::{Arc, RwLock};
+use std::collections::VecDeque;
 use minifb::{Key, Window, WindowOptions};
 use crate::object::renderable::Renderable;
 
@@ -57,17 +58,13 @@ fn main() {
 
         Box::new(object::Quad::new(&ds::Vector3::new(-1.0, -1.0, 6.0), &ds::Vector3::new(1.0, 0.0, 0.0), &ds::Vector3::new(0.0, 1.0, 0.0))),
 
-        Box::new(object::Sphere::new(&ds::Vector3::new(-0.75, -0.8, -5.0), 0.5)),
-        Box::new(object::Sphere::new(&ds::Vector3::new(0.75, -0.8, -5.0), 0.5)),
-        Box::new(object::Sphere::new(&ds::Vector3::new(0.0, 0.0, -5.0), 0.5)),
-        Box::new(object::Sphere::new(&ds::Vector3::new(0.0, 1.0, -5.0), 0.5)),
-        Box::new(object::Sphere::new(&ds::Vector3::new(0.0, 2.0, -5.0), 0.5))
+        Box::new(object::Sphere::new(&ds::Vector3::new(-3.25, -0.8, 6.0), 0.5)),
+        Box::new(object::Sphere::new(&ds::Vector3::new(-4.75, -0.8, 6.0), 0.5)),
+        Box::new(object::Sphere::new(&ds::Vector3::new(-4.0,   0.0, 6.0), 0.5)),
+        Box::new(object::Sphere::new(&ds::Vector3::new(-4.0,   1.0, 6.0), 0.5)),
+        Box::new(object::Sphere::new(&ds::Vector3::new(-4.0,   2.0, 6.0), 0.5))
     ];
 
-    minifbwindow(player, objects);
-}
-
-fn minifbwindow(player: object::Player, objects: Vec<Box<dyn object::Renderable + Send + Sync>>) {
     let mut window = Window::new(
         "Render Engine",
         WIDTH, HEIGHT,
@@ -83,6 +80,8 @@ fn minifbwindow(player: object::Player, objects: Vec<Box<dyn object::Renderable 
     let player = Arc::new(RwLock::new(player));
     let objects = Arc::new(objects);
     let mut deltatime = 0.0;
+    let mut ms_until_fps_update: i64 = 1000;
+    let mut fps: u128 = 0;
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         let start = std::time::Instant::now();
@@ -95,10 +94,11 @@ fn minifbwindow(player: object::Player, objects: Vec<Box<dyn object::Renderable 
 
             threads.push(thread::spawn(move || {
                 let mut pixels: Vec<u32> = vec![0; WIDTH * HEIGHT/thread_count];
+                let player_read = player_ref.read().unwrap();
 
                 for x in 0..WIDTH {
                     for y in ((HEIGHT/thread_count)*i)..(HEIGHT/thread_count)*(i+1) {
-                        pixels[(y - (HEIGHT/thread_count)*i) * WIDTH + x] = get_pixel_color(player_ref.read().unwrap().get_camera(), objects_ref.as_ref(), x as f64, y as f64)
+                        pixels[(y - (HEIGHT/thread_count)*i) * WIDTH + x] = get_pixel_color(player_read.get_camera(), objects_ref.as_ref(), x as f64, y as f64)
                     }
                 }
 
@@ -152,7 +152,14 @@ fn minifbwindow(player: object::Player, objects: Vec<Box<dyn object::Renderable 
 
         let elapsed = start.elapsed();
         deltatime = (elapsed.as_millis() as f64) / 1000.0;
+        ms_until_fps_update -= elapsed.as_millis() as i64;
+
+        if ms_until_fps_update <= 0 {
+            fps = 1000/elapsed.as_millis();
+            ms_until_fps_update = 1000;
+        }
+
         print!("\x1B[2J\x1B[1;1H");
-        println!("\n\n FPS: {}\n\n Time between frames: {}ms\n\n Camera position: {:?}\n\n Player Rotation: {:?}", 1000/elapsed.as_millis(), elapsed.as_millis(), player_mut.get_camera().pos(), player_mut.get_rotation());
+        println!("\n\n FPS: {}\n\n Time between frames: {}ms\n\n Camera position: {:?}\n\n Player Rotation: {:?}", fps, elapsed.as_millis(), player_mut.get_camera().pos(), player_mut.get_rotation());
     }
 }
