5436f5ec2009cad5f91b6e742ce53204c8d95353
prushton2 <peterrushton418@gmail.com>
2026-04-02 13:28:37 -0400

    added back ray for culling later

--- src/ds/mod.rs
+++ src/ds/mod.rs
@@ -2,5 +2,10 @@ pub mod vector3;
 pub mod math;
 // pub mod interval;
 // pub mod aabb;
+// pub mod ray
 
 pub use vector3::Vector3;
+
+// pub use interval::Interval;
+// pub use aabb::Aabb;
+// pub use ray::Ray;
---
+++ src/ds/ray.rs
@@ -0,0 +1,49 @@
+use crate::ds::Vector3;
+use auto_ops::*;
+
+#[derive(PartialEq, Copy, Clone)]
+pub struct Ray {
+    pub origin: Vector3,
+    pub direction: Vector3
+}
+
+impl Ray {
+    pub fn new(origin: &Vector3, direction: &Vector3) -> Self{
+        Self{
+            origin: origin.clone(),
+            direction: direction.clone()
+        }
+    }
+
+    pub fn at(&self, t: f64) -> Vector3 {
+        return self.origin + t * self.direction;
+    }
+}
+
+impl_op_ex!(+ |a: &Ray, b: &Ray| -> Ray{
+    Ray{
+        origin: a.origin + b.origin,
+        direction: a.direction + b.direction
+    }
+});
+
+impl_op_ex!(- |a: &Ray, b: &Ray| -> Ray{
+    Ray{
+        origin: a.origin - b.origin,
+        direction: a.direction - b.direction
+    }
+});
+
+impl_op_ex!(* |a: &Ray, b: &Ray| -> Ray{
+    Ray{
+        origin: a.origin * b.origin,
+        direction: a.direction * b.direction
+    }
+});
+
+impl_op_ex!(/ |a: &Ray, b: &Ray| -> Ray{
+    Ray{
+        origin: a.origin / b.origin,
+        direction: a.direction / b.direction
+    }
+});
